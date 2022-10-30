5af6b243e1af587a6f42ec42c091068d82b4db26
sara <work.sara10@gmail.com>
2022-10-30 21:03:38 +0700

    add transaction repo

--- src/main/usecases/transaction_repository.go
+++ src/main/usecases/transaction_repository.go
@@ -1,1 +1,18 @@
 package usecases
+
+import (
+	"service-api/src/main/entities"
+
+	"gorm.io/gorm"
+)
+
+type TransactionRepo interface {
+	FindAll() ([]entities.Transaction, error)
+	FindById(id uint) (entities.Transaction, error)
+	FindByUserId(userId uint) ([]entities.Transaction, error)
+	Save(transaction entities.Transaction) (entities.Transaction, error)
+}
+
+type transactionRepo struct {
+	db *gorm.DB
+}
