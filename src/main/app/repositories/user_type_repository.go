76729c23fce3f219bb409c897ebee74e602c5709
Prana <pranadhika22@gmail.com>
2022-10-31 19:32:45 +0700

    update user type
    add struct user type repo.

--- src/main/app/repositories/user_type_repository.go
+++ src/main/app/repositories/user_type_repository.go
@@ -1,10 +1,18 @@
 package repositories
 
-import "service-api/src/main/app/entities"
+import (
+	"service-api/src/main/app/entities"
+
+	"gorm.io/gorm"
+)
 
 type UserTypeRepo interface {
 	FindAll() ([]entities.UserType, error)
 	FindById(id uint) (entities.UserType, error)
 	FindByType(userType string) (entities.UserType, error)
 	Save(userType entities.UserType) (entities.UserType, error)
+}
+
+type userTypeRepo struct {
+	db *gorm.DB
 }
